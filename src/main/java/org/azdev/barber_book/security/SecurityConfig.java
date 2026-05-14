16fe3bf48fd9635ae155285ccea3c1a5ffbcd99f
Carlos3108 <c.silva3108@hotmail.com>
2026-05-13 21:59:43 -0300

    fix: ajustado cors

--- src/main/java/org/azdev/barber_book/security/SecurityConfig.java
+++ src/main/java/org/azdev/barber_book/security/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setExposedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
 
