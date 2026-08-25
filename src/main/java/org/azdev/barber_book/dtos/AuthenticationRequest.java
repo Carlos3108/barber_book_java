2da2bad9ab4ffc77440d053a4c2ff820f7f696ae
Carlos3108 <c.silva3108@hotmail.com>
2026-08-24 23:13:18 -0300

    feat: refactor dtos

--- src/main/java/org/azdev/barber_book/dtos/AuthenticationRequest.java
+++ src/main/java/org/azdev/barber_book/dtos/AuthenticationRequest.java
@@ -1,4 +1,14 @@
 package org.azdev.barber_book.dtos;
 
-public record AuthenticationRequest (String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthenticationRequest (
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        String password
+) {
 }
--- src/main/java/org/azdev/barber_book/dtos/RegisterRequest.java
+++ src/main/java/org/azdev/barber_book/dtos/RegisterRequest.java
@@ -1,4 +1,22 @@
 package org.azdev.barber_book.dtos;
 
-public record RegisterRequest(String shopName, String ownerName, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank(message = "O nome da barbearia é obrigatório")
+        String shopName,
+
+        @NotBlank(message = "O nome do proprietário é obrigatório")
+        String ownerName,
+
+        @NotBlank(message = "O e-mail é obrigatório")
+        @Email(message = "Formato de e-mail inválido")
+        String email,
+
+        @NotBlank(message = "A senha é obrigatória")
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres")
+        String password
+) {
 }
